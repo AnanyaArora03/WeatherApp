2ceb77fc21c33bb3bff644e1957cbaa601914ea1
AnanyaArora03 <109033695+AnanyaArora03@users.noreply.github.com>
2024-07-13 23:10:39 +0530

    Update utils.py

--- weather/utils.py
+++ weather/utils.py
@@ -1,5 +1,15 @@
 def extract_weather_info(weather_data):
-    city = weather_data['name']
-    temperature = weather_data['main']['temp']
-    description = weather_data['weather'][0]['description']
+
+ 
+    try:
+        city = weather_data['name']
+        temperature = weather_data['main']['temp']
+        description = weather_data['weather'][0]['description']
+        return city, temperature, description
+    except KeyError as e:
+        print(f"Error: Missing or unexpected data in API response - {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        
     return city, temperature, description
+    
